76f3d360e16a7cee0543c835ae089fe37ecf4c27
CHDamian <damian.chanko98@gmail.com>
2023-11-19 21:51:27 +0100

    Database created

---
+++ Gradja/GradjaApp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Roles, Users, Mails, Classes, Subjects, SubjectTypes, GradeType, GradeValue, Grades, ClassStudents
+
+# Register your models here.
+admin.site.register(Roles)
+admin.site.register(Users)
+admin.site.register(Mails)
+admin.site.register(Classes)
+admin.site.register(ClassStudents)
+admin.site.register(Subjects)
+admin.site.register(SubjectTypes)
+admin.site.register(Grades)
+admin.site.register(GradeType)
+admin.site.register(GradeValue)
+
---
+++ Gradja/GradjaApp/models.py
@@ -0,0 +1,118 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Roles(models.Model):
+    roleId = models.IntegerField(primary_key = True)
+    roleName = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "Roles"
+        verbose_name_plural = "Roles"
+
+class Users(models.Model):
+    userId = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    role = models.ForeignKey(Roles, on_delete= models.CASCADE)
+
+    studentParent = models.ManyToManyField('self', related_name='StudentParent', symmetrical=False)
+
+    class Meta:
+        db_table = "Users"
+        verbose_name_plural = "Users"
+
+class Mails(models.Model):
+    mailId = models.IntegerField(primary_key = True)
+    fromId = models.ForeignKey(Users, null = True, on_delete= models.SET_NULL, related_name= "fromId")
+    toId = models.ForeignKey(Users, null = True, on_delete= models.SET_NULL, related_name= "toId")
+    sendDate = models.DateField()
+    topic = models.CharField(max_length=200)
+    mailText = models.TextField()
+
+    class Meta:
+        db_table = "Mails"
+        verbose_name_plural = "Mails"
+
+class Classes(models.Model):
+    classId = models.IntegerField(primary_key = True)
+    className = models.CharField(max_length=10)
+    homeroomTeacher = models.ForeignKey(Users, null = True, on_delete= models.SET_NULL, related_name='homeroomTeacher')
+    activeFrom = models.DateField()
+    activeTo = models.DateField()
+
+    classStudent = models.ManyToManyField(Users, through='ClassStudents', symmetrical=False)
+
+    class Meta:
+        db_table = "Classes"
+        verbose_name_plural = "Classes"
+
+
+class ClassStudents(models.Model):
+    classId = models.ForeignKey(Classes, on_delete = models.CASCADE)
+    studentId = models.ForeignKey(Users, on_delete= models.CASCADE, related_name='studentId')
+    activeFrom = models.DateField()
+    activeTo = models.DateField()
+
+    class Meta:
+        db_table = "Class Students"
+        verbose_name_plural = "Class Students"
+
+
+class SubjectTypes(models.Model):
+    typeId = models.IntegerField(primary_key = True)
+    typeName = models.CharField(max_length=32)
+    description = models.TextField()
+
+    class Meta:
+        db_table = "Subject types"
+        verbose_name_plural = "Subject types"
+
+
+class Subjects(models.Model):
+    subjectId = models.IntegerField(primary_key = True)
+    classId = models.ForeignKey(Classes, on_delete = models.CASCADE)
+    subjectType = models.ForeignKey(SubjectTypes, on_delete = models.CASCADE)
+    teacherId = models.ForeignKey(Users, on_delete = models.CASCADE)
+    activeFrom = models.DateField()
+    activeTo = models.DateField()
+
+    class Meta:
+        db_table = "Subjects"
+        verbose_name_plural = "Subjects"
+
+
+class GradeType(models.Model):
+    typeId = models.IntegerField(primary_key = True)
+    typeName = models.CharField(max_length=20)
+    weight = models.IntegerField()
+
+    class Meta:
+        db_table = "Grade Types"
+        verbose_name_plural = "Grade Types"
+
+
+class GradeValue(models.Model):
+    gradeId = models.FloatField(primary_key = True)
+    typeName = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "Grade Values"
+        verbose_name_plural = "Grade Values"
+
+
+class Grades(models.Model):
+    gradeId = models.FloatField(primary_key = True)
+    classId = models.ForeignKey(Subjects, on_delete = models.CASCADE)
+    studentId = models.ForeignKey(Users, on_delete = models.CASCADE)
+    gradeId = models.ForeignKey(GradeValue, on_delete = models.CASCADE)
+    typeId = models.ForeignKey(GradeType, on_delete = models.CASCADE)
+    description = models.TextField()
+
+    class Meta:
+        db_table = "Grades"
+        verbose_name_plural = "Grades"
+
+
+
