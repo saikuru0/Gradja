15f1142f5b3ef857dac5f301b3880ed6894cc9a5
Isia07 <aga.p.10@wp.pl>
2023-12-17 01:26:00 +0100

    less comments

--- Gradja/GradjaApp/views.py
+++ Gradja/GradjaApp/views.py
@@ -1,5 +1,3 @@
-# from django.contrib import messages
-# from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -14,6 +12,7 @@ from .forms import SubjectTypeForm
 from .models import SubjectTypes
 from .models import Subjects
 from .forms import SubjectForm
+
 
 
 def home(request):
